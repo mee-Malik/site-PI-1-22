0857b20fde1c9f4a01bec5f8ff5de6c13917c97d
Daylet12 <ytt368537@gmail.com>
2024-12-27 21:47:08 +0600

    12add romb and add telegram icon1234

--- goods/admin.py
+++ goods/admin.py
@@ -6,6 +6,7 @@ from goods.models import Categories, Products
 # admin.site.register(Products)
 
 
+
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
